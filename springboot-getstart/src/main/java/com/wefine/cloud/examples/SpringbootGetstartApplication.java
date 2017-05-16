e376a13c34d997c4d6db0813ff82996ec7175323
wefine <wefine@aliyun.com>
2017-05-16 18:46:54 +0800

    day one finished!
    Signed-off-by: wefine <wefine@aliyun.com>

---
+++ springboot-getstart/src/main/java/com/wefine/cloud/examples/SpringbootGetstartApplication.java
@@ -0,0 +1,12 @@
+package com.wefine.cloud.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGetstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGetstartApplication.class, args);
+	}
+}
