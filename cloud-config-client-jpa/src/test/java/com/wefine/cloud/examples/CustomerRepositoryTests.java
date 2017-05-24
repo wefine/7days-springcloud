70389113c6df78c6d97b20df22ae845558d7d104
wefine <wefine@aliyun.com>
2017-05-24 16:41:24 +0800

    update cloud configuration test client
    Signed-off-by: wefine <wefine@aliyun.com>

--- cloud-config-client-jpa/src/test/java/com/wefine/cloud/examples/CustomerRepositoryTests.java
+++ cloud-config-client-jpa/src/test/java/com/wefine/cloud/examples/CustomerRepositoryTests.java
@@ -1,35 +1,22 @@
 
 package com.wefine.cloud.examples;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
-
-import com.wefine.cloud.examples.entity.Customer;
 import com.wefine.cloud.examples.repository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class CustomerRepositoryTests {
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
-    private CustomerRepository customers;
+    private CustomerRepository repository;
 
     @Test
     public void testFindByLastName() {
-        Customer customer = new Customer("first", "last");
-        entityManager.persist(customer);
-
-        List<Customer> findByLastName = customers.findByLastName(customer.getLastName());
-
-        assertThat(findByLastName).extracting(Customer::getLastName).containsOnly(customer.getLastName());
+        repository.findAll().forEach(System.out::println);
     }
 }
